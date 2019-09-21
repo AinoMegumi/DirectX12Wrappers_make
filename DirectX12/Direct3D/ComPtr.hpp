f6f6efa21ec85bff3eb5fab540d44dd3247ef285
AinoMegumi <aimegu2014@gmail.com>
2019-09-22 00:21:59 +0900

    MicrosoftのDxBaseをComPtrにクラス名を変更

--- DirectX12/Direct3D/ComPtr.hpp
+++ DirectX12/Direct3D/ComPtr.hpp
@@ -3,18 +3,16 @@
 #include <string>
 #include <functional>
 #include <stdexcept>
+template<class C> using MSComPtr = Microsoft::WRL::ComPtr<C>;
 
 namespace meigetsusoft {
-	namespace DirectX {
+	namespace COM {
 		template<class C>
-		class DxBase : public Microsoft::WRL::ComPtr<C> {
+		class ComPtr : public MSComPtr<C> {
 		public:
-			DxBase(const std::function<HRESULT(Microsoft::WRL::ComPtr<C>&)>& initFunction) {
+			ComPtr(const std::function<HRESULT(MSComPtr<C>&)>& initFunction) {
 				if (const HRESULT hr = initFunction(*this); FAILED(hr))
 					throw std::runtime_error("Failed to init " + std::string(typeid(C).name) + "\nErrorCode : " + std::to_string(hr));
-			}
-			DxBase(const std::function<void(Microsoft::WRL::ComPtr<C>&)>& initFunction) {
-				initFunction(*this);
 			}
 		};
 	}
