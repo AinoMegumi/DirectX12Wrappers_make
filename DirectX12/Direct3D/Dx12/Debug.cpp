03a827b705a680cd6f882d64a2b696965c7f08db
AinoMegumi <aimegu2014@gmail.com>
2019-09-22 01:30:30 +0900

    DebugControllerの有効化関数の作成

---
+++ DirectX12/Direct3D/Dx12/Debug.cpp
@@ -0,0 +1,25 @@
+﻿#include "Debug.hpp"
+#include <dxgi1_4.h>
+#include <dxgidebug.h>
+
+namespace meigetsusoft {
+	namespace DirectX {
+		namespace Dx12 {
+			DWORD EnableDebugController() noexcept {
+				DWORD DxgiFactoryFlags = 0;
+				MSComPtr<ID3D12Debug> debugController{};
+				if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(debugController.ReleaseAndGetAddressOf())))) {
+					debugController->EnableDebugLayer();
+					DxgiFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+					MSComPtr<IDXGIInfoQueue> DxgiInfoQueue{};
+					if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(DxgiInfoQueue.ReleaseAndGetAddressOf())))) {
+						DxgiInfoQueue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, true);
+						DxgiInfoQueue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, true);
+					}
+				}
+				else OutputDebugStringA("WARNING: Direct3D Debug Device is not available\n");
+				return DxgiFactoryFlags;
+			}
+		}
+	}
+}
---
+++ DirectX12/Direct3D/Dx12/Debug.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "../ComPtr.hpp"
+#include "d3dx12.h"
+
+namespace meigetsusoft {
+	namespace DirectX {
+		namespace Dx12 {
+			DWORD EnableDebugController() noexcept;
+		}
+	}
+}
