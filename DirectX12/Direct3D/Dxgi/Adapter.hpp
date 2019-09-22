46947c32d27f237fd3ad939bf14375ff8aab7505
AinoMegumi <aimegu2014@gmail.com>
2019-09-22 10:13:02 +0900

    機能レベルをメンバーから削除

--- DirectX12/Direct3D/Dxgi/Adapter.hpp
+++ DirectX12/Direct3D/Dxgi/Adapter.hpp
@@ -9,10 +9,10 @@ namespace meigetsusoft {
 			using WarpAdapter = COM::ComPtr<IDXGIAdapter>;
 
 			struct HardwareAdapterInformation : public DXGI_ADAPTER_DESC1 {
-				HardwareAdapterInformation(HardwareAdapter&& AdapterData, const DXGI_ADAPTER_DESC1& desc, const D3D_FEATURE_LEVEL& FeatureLevel)
-					: DXGI_ADAPTER_DESC1(std::move(desc)), Adapter(std::move(AdapterData)), SupportedFeatureLevel(FeatureLevel) {}
+				HardwareAdapterInformation() = default;
+				HardwareAdapterInformation(HardwareAdapter&& AdapterData, const DXGI_ADAPTER_DESC1& desc)
+					: DXGI_ADAPTER_DESC1(std::move(desc)), Adapter(std::move(AdapterData)) {}
 				HardwareAdapter Adapter;
-				D3D_FEATURE_LEVEL SupportedFeatureLevel;
 			};
 
 			std::vector<HardwareAdapterInformation> GetHardwareAdapters(const Factory& factory, const D3D_FEATURE_LEVEL& MinFeatureLevel = D3D_FEATURE_LEVEL_11_0);
