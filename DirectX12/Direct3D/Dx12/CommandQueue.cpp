3ce90e8ed45699b8e95ebbb0d9f0a1b9db09e05a
AinoMegumi <aimegu2014@gmail.com>
2019-09-23 19:29:18 +0900

    CommandQueueの初期化系を作成

---
+++ DirectX12/Direct3D/Dx12/CommandQueue.cpp
@@ -0,0 +1,20 @@
+﻿#include "CommandQueue.hpp"
+
+namespace meigetsusoft {
+	namespace DirectX {
+		namespace Dx12 {
+			D3D12_COMMAND_QUEUE_DESC CreateDefaultCommandQueueDesc() {
+				D3D12_COMMAND_QUEUE_DESC desc{};
+				desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+				desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+				return desc;
+			}
+
+			CommandQueue::CommandQueue(const Device& Dx12Device) 
+				: CommandQueue(Dx12Device, CreateDefaultCommandQueueDesc()) {}
+
+			CommandQueue::CommandQueue(const Device& Dx12Device, const D3D12_COMMAND_QUEUE_DESC& CmdQueueDesc) 
+				: COM::ComPtr<ID3D12CommandQueue>([&Dx12Device, &CmdQueueDesc](MSComPtr<ID3D12CommandQueue>& CmdQueue) { return Dx12Device->CreateCommandQueue(&CmdQueueDesc, IID_PPV_ARGS(CmdQueue.ReleaseAndGetAddressOf())); }) {}
+		}
+	}
+}
---
+++ DirectX12/Direct3D/Dx12/CommandQueue.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Device.hpp"
+
+namespace meigetsusoft {
+	namespace DirectX {
+		namespace Dx12 {
+			class CommandQueue : public COM::ComPtr<ID3D12CommandQueue> {
+			public:
+				CommandQueue(const Device& Dx12Device);
+				CommandQueue(const Device& Dx12Device, const D3D12_COMMAND_QUEUE_DESC& CmdQueueDesc);
+			};
+		}
+	}
+}
